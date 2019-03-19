457726b32fb31dda528cb81b777f2001627a23c0
Mariliayovana <marilia_yovana@hotmail.com>
2019-03-19 09:52:09 -0500

    usando window

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 /* Manejo de data 
+
 const example = () => {
   return 'example';
 };
 
 window.example = example;
-
 */
 
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const dataSteam = window.STEAM.appnews;
-console.log(dataSteam);
+
+const dataSteam =window.STEAM.appnews;
