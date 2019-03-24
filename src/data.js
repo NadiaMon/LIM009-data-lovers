5f2b9162c166492e0db1929c084392b5f05a7768
NadiaMon <arana.mo89@gmail.com>
2019-03-24 12:38:03 -0500

    implementando método sort, ordenar fechas de manera ascendente

--- src/data.js
+++ src/data.js
@@ -26,63 +26,98 @@ for(let i=0; i<dataSteam.length;i++){
 
 const categorias = () => {
 
-  const filtrado = {}
+    const filtrado = {};
 
-  const productUpdate = dataSteam.filter((label) => {
-      return (label.feedlabel === "Product Update");
-  })
+    const productUpdate = dataSteam.filter((label) => {
+        return (label.feedlabel === "Product Update");
+    })
 
-  filtrado.producto = productUpdate;
+    filtrado.producto = productUpdate;
 
-  const pcGamer = dataSteam.filter((gamer) => {
-      return (gamer.feedlabel === "PC Gamer");
-  })
+    const pcGamer = dataSteam.filter((gamer) => {
+        return (gamer.feedlabel === "PC Gamer");
+    })
 
-  filtrado.gamer = pcGamer;
+    filtrado.gamer = pcGamer;
 
-  const euroGamer = dataSteam.filter((euro) => {
-      return (euro.feedlabel === "Eurogamer");
-  })
+    const euroGamer = dataSteam.filter((euro) => {
+        return (euro.feedlabel === "Eurogamer");
+    })
 
-  filtrado.euro = euroGamer;
+    filtrado.euro = euroGamer;
 
-  const tfBlog = dataSteam.filter((blog) => {
-      return (blog.feedlabel === "TF2 Blog");
-  })
+    const tfBlog = dataSteam.filter((blog) => {
+        return (blog.feedlabel === "TF2 Blog");
+    })
 
-  filtrado.blog = tfBlog;
+    filtrado.blog = tfBlog;
 
-  const rockPaperShotgun = dataSteam.filter((rock) => {
-      return (rock.feedlabel === "Rock, Paper, Shotgun");
-  })
+    const rockPaperShotgun = dataSteam.filter((rock) => {
+        return (rock.feedlabel === "Rock, Paper, Shotgun");
+    })
 
-  filtrado.rock = rockPaperShotgun;
+    filtrado.rock = rockPaperShotgun;
 
-  return filtrado;
+    return filtrado;
 }
 
 window.categorias = categorias;
 
 //timestamp convert
 
-const fechaconvertTimestamp = (arr) => {
-
-    for (i = 0; i < arr.length; i++) {
-        let fecha = arr[i]["date"];
-        let convertir = (fecha * 1000);
-
-    }
-    
 
 
+
+/*
+
+const fechaconvertTimestamp = () => {
+
+    for (i = 0; i < dataSteam.length; i++) {
+        
+        let convertir = new Date(date * 1000);
+        console.log(convertir);
+   }
 }
 
-// console.log(new Date(1540254060 * 1000));
+*/
 
 
+console.log(new Date(1542738000 * 1000))
+console.log(new Date(1540923360 * 1000))
+console.log(new Date(1540923360 * 1000))
+console.log(new Date(1540494720 * 1000))
+console.log(new Date(1540494720 * 1000))
+console.log(new Date(1540482175 * 1000))
+console.log(new Date(1540464693 * 1000))
+console.log(new Date(1540295862 * 1000))
+console.log(new Date(1540254060 * 1000))
+console.log(new Date(1540254060 * 1000))
 
 
+// const fechaconvertTimestamp = () => {
 
+//     const nuevaFecha = {};
+//     for (x = 0; dataSteam.length; x++) {
+//         return (new Date(dataSteam[i].date * 1000))
 
+//         nuevaFecha.fecha = fechaconvertTimestamp;
+//     }
+//      return nuevaFecha
+
+// }
+
+//para ordenar la data por fechas de manera ascendente
+
+const ordenarFechas = dataSteam.sort((a,b) => {
+    let dateA = new Date((a.date) * 1000);
+    let dateB = new Date((b.date) * 1000);
+
+    return dateA - dateB;
+
+})
+
+console.log(ordenarFechas);
+
+//para ordenar la data por fechas de manera ascendente
 
 
