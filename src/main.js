8ef98be73d6c40afc0150ba46f5930ca3d2bfc71
Mariliayovana <marilia_yovana@hotmail.com>
2019-03-26 22:26:03 -0500

    agregar comentarios

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-const filtrarCategorias= steam.categorias(dataSteam); //aqui llamo a mi data
+const filtrarCategorias= steam.categorias(dataSteam); //devuelve un objeto donde cada atributo es un array no noticias por categoria
 const mostrarData = document.getElementById("pantalla-muestra");  //section donde se imprimira la data
 
 const noticiaTemplate = (data) =>{//data = [{url, titulo, contenido}, {}, {}]
     let item = `
-        <a href="${data.url}" target="_blank" class="titulo-noticia">${data.title}</a>
-        <p>${data.author}</p>
-        <p>${data.contents}</p>
+        <div id="noticia">
+            <a href="${data.url}" target="_blank" class="titulo-noticia">${data.title}</a>
+            <p>${data.author}</p>
+            <p>${data.contents}</p>
+        </div>
     `;
 
     return item;
@@ -13,18 +15,26 @@ const noticiaTemplate = (data) =>{//data = [{url, titulo, contenido}, {}, {}]
 //funcion para el boton de filtrado
 
 const pintarNoticias = (noticias) => {
+    //elimina las anteriores noticias
     mostrarData.innerHTML = "";
+
+    //por cada noticia
     for (let i = 0; i < noticias.length; i++){
+        //obtiene el html de la noticia
         const noticia = noticiaTemplate(noticias[i])
+
+        //pinta el html En la pagina
         mostrarData.insertAdjacentHTML('beforeend', noticia);
     }
 }
 
+//primera carga
 const init = () => {
     pintarNoticias(dataSteam);
 }
 
 const btnProducto = document.getElementById("btn-product");
+
 btnProducto.addEventListener("click", () => {
     pintarNoticias(filtrarCategorias.producto);
 })
