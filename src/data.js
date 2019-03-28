c51b880845c72e6fb5842aadeaa74acb51b55e27
Mariliayovana <marilia_yovana@hotmail.com>
2019-03-27 22:34:18 -0500

    filtro por fecha

--- src/data.js
+++ src/data.js
@@ -1,49 +1,49 @@
 const dataSteam = STEAM.appnews.newsitems;
 
 const dataMuestra = (data) => { //nuevo data para mostrar
-   const arr = [];
-   for (let i = 0; i < data.length; i++) {
-       arr.push({title: data[i].title, url: data[i].url, contents: data[i].contents});
-   }
-   return arr;
+    const arr = [];
+    for (let i = 0; i < data.length; i++) {
+        arr.push({ title: data[i].title, url: data[i].url, contents: data[i].contents });
+    }
+    return arr;
 }
 
 //filtrado dentro de una función
 const categorias = (data) => { //data = [{ }, { }]
 
-   const filtrado = {};
+    const filtrado = {};
 
-   const productUpdate = data.filter((label) => { //usando filter para separar data por canales
-       return (label.feedlabel === "Product Update");
-   })
+    const productUpdate = data.filter((label) => { //usando filter para separar data por canales
+        return (label.feedlabel === "Product Update");
+    })
 
-   filtrado.producto = productUpdate; //propiedad:value que se agregan a filtrado
+    filtrado.producto = productUpdate; //propiedad:value que se agregan a filtrado
 
-   const pcGamer = data.filter((gamer) => {
-       return (gamer.feedlabel === "PC Gamer");
-   })
+    const pcGamer = data.filter((gamer) => {
+        return (gamer.feedlabel === "PC Gamer");
+    })
 
-   filtrado.gamer = pcGamer;
+    filtrado.gamer = pcGamer;
 
-   const euroGamer = data.filter((euro) => {
-       return (euro.feedlabel === "Eurogamer");
-   })
+    const euroGamer = data.filter((euro) => {
+        return (euro.feedlabel === "Eurogamer");
+    })
 
-   filtrado.euro = euroGamer;
+    filtrado.euro = euroGamer;
 
-   const tfBlog = data.filter((blog) => {
-       return (blog.feedlabel === "TF2 Blog");
-   })
+    const tfBlog = data.filter((blog) => {
+        return (blog.feedlabel === "TF2 Blog");
+    })
 
-   filtrado.blog = tfBlog;
+    filtrado.blog = tfBlog;
 
-   const rockPaperShotgun = data.filter((rock) => {
-       return (rock.feedlabel === "Rock, Paper, Shotgun");
-   })
+    const rockPaperShotgun = data.filter((rock) => {
+        return (rock.feedlabel === "Rock, Paper, Shotgun");
+    })
 
-   filtrado.rock = rockPaperShotgun;
+    filtrado.rock = rockPaperShotgun;
 
-   return filtrado;
+    return filtrado;
 }
 
 //creando una función que devuelva la fecha de cada noticia
@@ -75,75 +75,51 @@ console.log(new Date(1540254060 * 1000))
 console.log(new Date(1540254060 * 1000))
 */
 
+    // const fechas = dataSteam.sort((a,b) => {
+    //    let dateA = new Date((a.date) * 1000); //convertir el timestamp a milisegundos
+    //    let dateB = new Date((b.date) * 1000);
 
-/*
-const fechas = dataSteam.sort((a,b) => {
-   let dateA = new Date((a.date) * 1000); //convertir el timestamp a milisegundos
-   let dateB = new Date((b.date) * 1000);
+    //    if(dateA > dateB){
+    //        return 1;
+    //    }
+    //    if(dateA < dateB){
+    //        return -1;
+    //    }
 
-   if(dateA > dateB){
-       return 1;
-   }
-   if(dateA < dateB){
-       return -1;
-   }
+    //    return 0;
+    // })
 
-   return 0;
-})
-*/
+    // const ordenarPorFechas =(noticias)=> {
 
+    //esto es las fechas de mas antiguo a nuevo
+    const fechasAscendente = (noticias) => {
+        noticias.sort((a, b) => {
+            let dateA = new Date((a.date) * 1000); //convertir el timestamp a milisegundos
+            let dateB = new Date((b.date) * 1000);
+            
+            return dateA - dateB;
+        })
 
+        return noticias;
+    } 
 
+    //para ordenar la data por fechas de manera ascendente
 
-/*
-const fechasAscendente = dataSteam.sort((a,b) => {
-   let dateA = new Date((a.date) * 1000); //convertir el timestamp a milisegundos
-   let dateB = new Date((b.date) * 1000);
+    const fechasDescendente =(noticias) =>{
+        noticias.sort((a, b) => {
+            let dateA = new Date((a.date) * 1000);
+            let dateB = new Date((b.date) * 1000);
 
-   return dateA - dateB;
+            return dateB - dateA;
+        })
 
-})
+        return noticias;
+    }
 
-console.log(fechasAscendente);
+    window.steam = {  //objeto que contiene todos los métodos
+        categorias: categorias,
+        fechasAscendente:fechasAscendente,
+        fechasDescendente:fechasDescendente,
+        dataMuestra: dataMuestra
 
-//para ordenar la data por fechas de manera ascendente
-
-const fechasDescendente = dataSteam.sort((a,b) => {
-   let dateA = new Date((a.date) * 1000);
-   let dateB = new Date((b.date) * 1000);
-
-   return dateB - dateA;
-
-})
-
-console.log(fechasDescendente);
-
-
-
-
-
-/*
-//ordenar los títulos
-
-const ordenandoTitulos = dataSteam.sort((a,b) => {
-
-
-
-   let dateA = new Date((a.date) * 1000);
-   let dateB = new Date((b.date) * 1000);
-
-   return dateB - dateA;
-
-})
-
-console.log(fechasDescendente);
-
-*/
-
-
-window.steam = {  //objeto que contiene todos los métodos
-    categorias: categorias,
-    //fechaconvertTimestamp: fechaconvertTimestamp,
-    dataMuestra: dataMuestra
-
-}
+    }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-const filtrarCategorias= steam.categorias(dataSteam); //devuelve un objeto donde cada atributo es un array no noticias por categoria
+const filtrarCategorias= steam.categorias(dataSteam); //[{}]//devuelve un objeto donde cada atributo es un array no noticias por categoria
 const mostrarData = document.getElementById("pantalla-muestra");  //section donde se imprimira la data
+const nombreFecha= document.getElementById("ordenar-fecha");
+let noticiasMostradas = [];
 
+//El pedacito de html de la noticia a mostrar
 const noticiaTemplate = (data) =>{//data = [{url, titulo, contenido}, {}, {}]
     let item = `
         <div id="noticia">
@@ -12,10 +15,32 @@ const noticiaTemplate = (data) =>{//data = [{url, titulo, contenido}, {}, {}]
 
     return item;
 };
-//funcion para el boton de filtrado
 
+const mostrarNoticiasOrdenado = (noticias) => {
+    //variable para guardar las noticiar ordenadas por fecha para luwego mostrarlas
+    let noticiasOrden = [];
+      
+    //nombreFecha.value devuelve el valor del desplegable en el html
+    //verificar el tipo de orden por hacer
+    if (nombreFecha.value == 'opcion1') {
+        //ordenar de recientes
+        noticiasOrden = steam.fechasDescendente(noticias)
+    }
+    else {
+         //ordenar de pasada
+         noticiasOrden = steam.fechasAscendente(noticias)
+    }
+    pintarNoticias(noticiasOrden);
+}
+
+nombreFecha.addEventListener("change",()=>{
+   mostrarNoticiasOrdenado(noticiasMostradas)
+})
+
+//funcion para el boton de filtrado
 const pintarNoticias = (noticias) => {
-    //elimina las anteriores noticias
+    noticiasMostradas = noticias;
+    //elimina las  noticias anteriores y me muestra las que selecciono
     mostrarData.innerHTML = "";
 
     //por cada noticia
@@ -23,41 +48,48 @@ const pintarNoticias = (noticias) => {
         //obtiene el html de la noticia
         const noticia = noticiaTemplate(noticias[i])
 
-        //pinta el html En la pagina
+        //pinta el html En la pagina 
+        //inserta el html de la noticia al final de la pagina
         mostrarData.insertAdjacentHTML('beforeend', noticia);
     }
 }
 
 //primera carga
 const init = () => {
-    pintarNoticias(dataSteam);
+    mostrarNoticiasOrdenado(dataSteam);
 }
 
 const btnProducto = document.getElementById("btn-product");
 
 btnProducto.addEventListener("click", () => {
-    pintarNoticias(filtrarCategorias.producto);
+    mostrarNoticiasOrdenado(filtrarCategorias.producto);
 })
 
 const btnGamer = document.getElementById("btn-gamer");
 btnGamer.addEventListener("click", () => {
-    pintarNoticias(filtrarCategorias.gamer);
+    mostrarNoticiasOrdenado(filtrarCategorias.gamer);
 })
 
 
 const btnEuro = document.getElementById("btn-euro");
 btnEuro.addEventListener("click", () => {
-    pintarNoticias(filtrarCategorias.euro);
+    mostrarNoticiasOrdenado(filtrarCategorias.euro);
 })
 
 const btnBlog = document.getElementById("btn-blog");
 btnBlog.addEventListener("click", () => {
-    pintarNoticias(filtrarCategorias.blog);
+    mostrarNoticiasOrdenado(filtrarCategorias.blog);
 })
 
 const btnRock = document.getElementById("btn-rock");
 btnRock.addEventListener("click", () => {
-    pintarNoticias(filtrarCategorias.rock);
+    mostrarNoticiasOrdenado(filtrarCategorias.rock);
 })
 
 init();
+
+
+
+ 
+
+ 
