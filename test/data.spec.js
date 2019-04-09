9f98e27dddecbf18c1148f4cb862663b60d3617c
NadiaMon <arana.mo89@gmail.com>
2019-04-09 12:03:37 -0500

    corrigiendo errores

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,16 +1,3 @@
 global.window = global;
-global.assert = require('chai').assert;
-require('../src/data');
+require('../src/data.js');
 require('./data.spec.js');
-
-
-describe('example', () => {
-  
-  it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
-  });
-
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
-  });
-})
